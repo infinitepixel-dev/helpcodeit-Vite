30f32e3336779129e7036d05a745e786b00c8566
Masheen88 <mattdrummerguy@gmail.com>
2024-02-06 09:38:06 -0600

    Added Notes:

--- src/components/CSS_Wrappers/CSS_Main_Profile.jsx
+++ src/components/CSS_Wrappers/CSS_Main_Profile.jsx
@@ -4,38 +4,45 @@ function TW_CSS() {
     "(prefers-color-scheme: dark)"
   ).matches;
 
-  prefersDarkMode = true;
+  prefersDarkMode = false;
 
   console.log("Dark Mode: ", prefersDarkMode);
 
-  // Define style objects
+  //SECTION[MainCards.jsx] - MainCards Component
+
+  //NOTE Main Card Body
   let mainCardBody = {
     default: "w-full mx-auto my-8",
   };
 
+  //NOTE Main Card Border
   let mainCardBorder = {
     default: "bg-white shadow-xl border border-black rounded card-max",
-    light: "bg-white shadow-xl border border-black rounded card-max",
+    // light: "bg-white shadow-xl border border-black rounded card-max",
     dark: "dark:bg-neutral-600 shadow-xl border border-black rounded card-max",
   };
 
+  //NOTE Main Card Image
   let mainCardImage = {
     default: "w-full h-auto border-b-2 border-black",
-    light: "w-full h-auto border-b-2 border-black",
-    dark: "w-full h-auto border-b-2 border-black",
+    // light: "w-full h-auto border-b-2 border-black",
+    // dark: "w-full h-auto border-b-2 border-black",
   };
 
+  //NOTE Main Card Title
   let mainCardTitle = {
     default: "text-4xl pb-8 font-bold text-black",
-    light: "text-4xl pb-8 font-bold text-black",
+    // light: "text-4xl pb-8 font-bold text-black",
     dark: "text-4xl pb-8 font-bold dark:text-white",
   };
 
+  //NOTE Main Card Description
   let mainCardDesc = {
     default: "text-black text-xl",
-    light: "text-black text-xl",
+    // light: "text-black text-xl",
     dark: "dark:text-white text-xl ",
   };
+  //!SECTION[MainCards.jsx] - MainCards Component
 
   // Determine the mode based on prefersDarkMode
   const mode = prefersDarkMode ? "dark" : "light";
