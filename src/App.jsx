e4e8917a10bfd8e3fe1dba488b68922716a69c84
Masheen88 <mattdrummerguy@gmail.com>
2024-02-12 14:40:01 -0600

    Add CodePractice component

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,32 @@ import About from "./components/Pages/About";
 import JavascriptMainPage from "./components/Pages/JavascriptMainPage";
 import PracticeProblems from "./components/Pages/javascriptPrincipals/practiceproblems";
 import DeclaringVariables from "./components/Pages/javascriptPrincipals/DeclaringVariables";
+import CodePractice from "./components/Sub_Components/Code_Practice";
 
 export default function App() {
-
   return (
     <DarkModeProvider>
       <div>
-        <div className='w-full m-0 p-0'>
-  
+        <div className="w-full m-0 p-0">
           <Navbar />
         </div>
 
         <Routes>
-          <Route exact path='/' element={<HomePage />} />
+          <Route exact path="/" element={<HomePage />} />
           {/* <Route path='/GettingStarted' element={<GettingStarted />} /> */}
-          <Route exact path='/about' element={<About />} />
-          <Route path='/javascript' element={<JavascriptMainPage />} />
+          <Route exact path="/about" element={<About />} />
+          <Route path="/javascript" element={<JavascriptMainPage />} />
           <Route
-            path='/javascriptPrincipals/practiceproblems'
+            path="/javascriptPrincipals/practiceproblems"
             element={<PracticeProblems />}
           />
           <Route
-            path='/javascriptPrincipals/declaringvariables'
+            path="/javascriptPrincipals/declaringvariables"
             element={<DeclaringVariables />}
           />
           {/*<Route path='/contact' element={<Contact />} />
         <Route path='*' element={<NotFound />} /> */}
+          <Route path="/code_practice" element={<CodePractice />} />
         </Routes>
       </div>
     </DarkModeProvider>
