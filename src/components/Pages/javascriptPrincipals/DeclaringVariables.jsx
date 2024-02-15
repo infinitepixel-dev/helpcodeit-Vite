2f8f4be051061aa9f4a27a4d69295984811d402c
movarnell <movarnell@gmail.com>
2024-02-15 13:39:02 -0600

    Add new dependencies and update tailwind styles

--- src/components/Pages/javascriptPrincipals/DeclaringVariables.jsx
+++ src/components/Pages/javascriptPrincipals/DeclaringVariables.jsx
@@ -1,6 +1,10 @@
 import hljs from "highlight.js";
 import { useEffect, useRef } from "react";
-
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "../../ui/hover-card";
 
 export default function DeclaringVariables() {
   const codeRef = useRef(null);
@@ -34,6 +38,7 @@ export default function DeclaringVariables() {
             title='YouTube video player'
             frameBorder='0'
             allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            allowFullScreen
           ></iframe>
         </div>
 
@@ -43,17 +48,45 @@ export default function DeclaringVariables() {
           <p className='text-lg'>
             In JavaScript, var is a keyword used to declare a variable. When you
             declare a variable using var, it is function-scoped, meaning it is
-            available anywhere in the function where it was declared. If it's
-            declared outside of any function, it's globally scoped. It's
-            important to note that variables declared with var can be
-            re-declared and updated, which can lead to potential issues with
+            available anywhere in the function where it was declared.{" "}
+            <HoverCard>
+              <HoverCardTrigger>
+                <span className='text-blue-800  underline font-extrabold dark:text-blue-400'>
+                  Confused about Scope?
+                </span>
+              </HoverCardTrigger>
+              <HoverCardContent
+                className='bg-slate-200  m-5 border border-black font-extrabold text-lg shadow-lg px-10'
+                customClass=' bg-slate-200  dark:bg-gray-700'
+              >
+                Scope in programming is like a set of boundaries that determines
+                where variables and functions are accessible within your code.
+                Think of it as an imaginary fence that encloses variables and
+                functions, deciding where they can be seen or used. For example,
+                a variable declared inside a function is like an item inside a
+                locked room – it's only available within that function. On the
+                other hand, a variable declared outside of all functions is like
+                an item left in a public park – any part of your program can
+                access it. Understanding scope is crucial because it helps
+                manage and isolate variables in different parts of your code,
+                preventing unexpected errors and conflicts.
+              </HoverCardContent>
+            </HoverCard>
+            &nbsp;If it's declared outside of any function, it's globally
+            scoped. It's important to note that variables declared with var can
+            be re-declared and updated, which can lead to potential issues with
             variable scope, especially in larger codebases. This is why in
             modern JavaScript, let (for block-scoped variables that can be
             reassigned) and const (for block-scoped variables that cannot be
             reassigned) are often preferred over var. <br />
             Here's an example:
           </p>
-
+          <div className='hover-container'>
+            Hover over me
+            <div className='hover-card'>
+              This is the content of the hover card.
+            </div>
+          </div>
           <pre>
             <code ref={codeRef} className='language-javascript my-3 p-2'>
               {varExample}
@@ -69,8 +102,8 @@ export default function DeclaringVariables() {
           <p>
             In JavaScript, let is used to create a variable that you can change
             later. It's similar to var, but let is more specific about where the
-            variable can be used in your code. This is called Scope.
-            This makes your code safer and easier to understand.
+            variable can be used in your code. This is called Scope. This makes
+            your code safer and easier to understand.
             <br /> Here's an example:
           </p>
           <pre>
