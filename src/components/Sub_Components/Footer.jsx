9882c2b4f451aa4a589d24b4931c47ad7015ee84
Masheen88 <mattdrummerguy@gmail.com>
2024-03-06 18:38:21 -0600

    misc change

--- src/components/Sub_Components/Footer.jsx
+++ src/components/Sub_Components/Footer.jsx
@@ -1,36 +1,34 @@
 function Footer() {
     return (
-        <div className="fixed bottom-0 w-full flex items-end">
-        <div className="fixed bottom-0 mt-5 w-full">
-
-            <footer className="bg-gray-200 p-5 dark:bg-gray-800">
-                <div className="flex justify-center space-x-4">
-                    {/* copywrite Michael Varnell statement */}
-                    <p className="text-lg font-bold">
-                        &copy; 2024{' '}
-                        <a
-                            href="https://michaelvarnell.com"
-                            target="_blank"
-                            className="underline hover:text-blue-500"
-                            rel="noreferrer"
-                        >
-                            Michael Varnell
-                        </a>{' '}
-                        &
-                        <a
-                            href="https://www.linkedin.com/in/matthewthomascox/"
-                            target="_blank"
-                            className="underline hover:text-blue-500"
-                            rel="noreferrer"
-                        >
-                            Matthew Cox
-                        </a>
-                    </p>
-                </div>
-            </footer>
+        <div className="fixed bottom-0 flex w-full items-end">
+            <div className="fixed bottom-0 mt-5 w-full">
+                <footer className="bg-gray-200 p-5 dark:bg-gray-800">
+                    <div className="flex justify-center space-x-4">
+                        {/* copywrite Michael Varnell statement */}
+                        <p className="text-lg font-bold">
+                            &copy; 2024{' '}
+                            <a
+                                href="https://michaelvarnell.com"
+                                target="_blank"
+                                className="underline hover:text-blue-500"
+                                rel="noreferrer"
+                            >
+                                Michael Varnell
+                            </a>{' '}
+                            &
+                            <a
+                                href="https://www.linkedin.com/in/matthewthomascox/"
+                                target="_blank"
+                                className="underline hover:text-blue-500"
+                                rel="noreferrer"
+                            >
+                                Matthew Cox
+                            </a>
+                        </p>
+                    </div>
+                </footer>
+            </div>
         </div>
-    </div>
-           
     )
 }
 export default Footer
