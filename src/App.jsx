99179a18f2220db3e96e26c1f02c4c6a2a532f2c
movarnell <movarnell@gmail.com>
2024-07-03 10:32:24 -0500

    adding war game page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import hljs from 'highlight.js'
 import 'highlight.js/styles/github-dark.css'
 import { Routes, Route } from 'react-router-dom'
 import Footer from './components/Sub_Components/Footer'
+import WarGameTutorial from './components/Pages/javascriptPrincipals/WarGameTutorial'
 
 // Lazy loading components
 const HomePage = lazy(() => import('./components/Pages/HomePage'))
@@ -86,6 +87,9 @@ const VSCodeHotkeysTable = lazy(
 )
 const MenuApp = lazy(
     () => import('./components/Pages/javascriptPrincipals/MenuApp')
+)
+const WarGame = lazy(
+    () => import('./components/Pages/javascriptPrincipals/WarGameTutorial')
 )
 
 export default function App() {
@@ -179,6 +183,7 @@ export default function App() {
         { path: '/javascriptPrincipals/CRUD-AJAX', component: <AJAX /> },
         { path: '/javascriptPrincipals/CRUD-Fetch', component: <Fetch /> },
         { path: '/javascriptPrincipals/MenuApp', component: <MenuApp /> },
+        {path: '/javascriptPrincipals/WarGameTutorial', component: <WarGameTutorial />},
 
         // React
         { path: '/ReactMain', component: <ReactMain /> },
