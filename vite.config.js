7dd755c99c5eb36b7445412f3573322d5b093a3d
Masheen88 <mattdrummerguy@gmail.com>
2024-08-06 05:49:17 -0500

    Updated Sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vitejs.dev/config/
+// Define __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@subComponents': path.resolve(__dirname, 'src/components/Sub_Components'),
-      '@JSP': path.resolve(__dirname, 'src/components/Pages/javascriptPrincipals'),
-      '@react': path.resolve(__dirname, 'src/components/Pages/react'),
-      '@fundamentals': path.resolve(__dirname, 'src/components/Pages/fundamentals'),
-    }
-  }
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@subComponents': path.resolve(
+                __dirname,
+                'src/components/Sub_Components'
+            ),
+            '@JSP': path.resolve(
+                __dirname,
+                'src/components/Pages/javascriptPrincipals'
+            ),
+            '@react': path.resolve(__dirname, 'src/components/Pages/react'),
+            '@fundamentals': path.resolve(
+                __dirname,
+                'src/components/Pages/fundamentals'
+            ),
+        },
+    },
+    build: {
+        outDir: 'dist',
+    },
 })
