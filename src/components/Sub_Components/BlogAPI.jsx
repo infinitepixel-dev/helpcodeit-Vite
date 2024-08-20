5fa8c6a49033a2b41138fd1adbca4e9763856df6
Masheen88 <mattdrummerguy@gmail.com>
2024-08-20 10:49:27 -0500

    removed redundant code

--- src/components/Sub_Components/BlogAPI.jsx
+++ src/components/Sub_Components/BlogAPI.jsx
@@ -1,32 +1,38 @@
-import React, { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect } from 'react'
 
-const VITE_BLOG_API_KEY = import.meta.env.VITE_BLOG_API_KEY;
-const VITE_BLOG_SPACE_ID = import.meta.env.VITE_BLOG_SPACE_ID;
-const BLOG_URL = `https://cdn.contentful.com/spaces/${VITE_BLOG_SPACE_ID}/environments/master/entries?access_token=${VITE_BLOG_API_KEY}`;
+import propTypes from 'prop-types'
 
-export const BlogContext = createContext();
+const VITE_BLOG_API_KEY = import.meta.env.VITE_BLOG_API_KEY
+const VITE_BLOG_SPACE_ID = import.meta.env.VITE_BLOG_SPACE_ID
+const BLOG_URL = `https://cdn.contentful.com/spaces/${VITE_BLOG_SPACE_ID}/environments/master/entries?access_token=${VITE_BLOG_API_KEY}`
+
+export const BlogContext = createContext()
 
 export const BlogProvider = ({ children }) => {
-    const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState([])
 
     const getPosts = async () => {
         try {
-            const response = await fetch(BLOG_URL);
-            const data = await response.json();
-            console.log("Blog Data:", data);
-            setPosts(data.items);  // Assuming data.items is an array of posts
+            const response = await fetch(BLOG_URL)
+            const data = await response.json()
+            console.log('Blog Data:', data)
+            setPosts(data.items) // Assuming data.items is an array of posts
         } catch (error) {
-            console.error('Error fetching posts:', error);
+            console.error('Error fetching posts:', error)
         }
-    };
+    }
 
     useEffect(() => {
-        getPosts();  // Fetch posts when the component mounts
-    }, []);
+        getPosts() // Fetch posts when the component mounts
+    }, [])
 
     return (
         <BlogContext.Provider value={{ posts, setPosts }}>
             {children}
         </BlogContext.Provider>
-    );
-};
+    )
+}
+
+BlogProvider.propTypes = {
+    children: propTypes.node,
+}
