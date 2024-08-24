705e7778b0fb1e7ed49a55a228257d5af0c61af3
movarnell <michaelvarnell@icloud.com>
2024-08-23 21:15:21 -0500

    Update BlogsPage component to fix error in splitting the text for preview text.

--- src/components/Pages/BlogsPage.jsx
+++ src/components/Pages/BlogsPage.jsx
@@ -16,20 +16,24 @@ function BlogsPage() {
         let previewText = ''
         let wordCount = 0
 
-        for (let i = 0; i < posts.content.length; i++) {
-            const contentBlock = posts.content[i].content
+             for (let i = 0; i < posts.content.length; i++) {
+            const contentBlock = posts.content[i].content;
             for (let j = 0; j < contentBlock.length; j++) {
-                const text = contentBlock[j].value
-                const words = text.split(' ')
+                const text = contentBlock[j].value;
+
+                if (typeof text !== 'string') {
+                    continue; // Skip if text is not a string
+                }
+
+                const words = text.split(' ');
 
                 if (wordCount + words.length > 100) {
-                    const remainingWords = 100 - wordCount
-                    previewText +=
-                        words.slice(0, remainingWords).join(' ') + '...'
-                    return previewText
+                    const remainingWords = 100 - wordCount;
+                    previewText += words.slice(0, remainingWords).join(' ') + '...';
+                    return previewText;
                 } else {
-                    previewText += text + ' '
-                    wordCount += words.length
+                    previewText += text + ' ';
+                    wordCount += words.length;
                 }
             }
         }
@@ -39,14 +43,14 @@ function BlogsPage() {
 
     return (
         <div>
-            <h1 className="satisfyFont my-8 text-center text-7xl">
+            <h1 className="my-8 text-center satisfyFont text-7xl">
                 Code Chronicles
             </h1>
-            <p className="text-center text-3xl">
+            <p className="text-3xl text-center">
                 Welcome to the Help Code It blog{' '}
             </p>
             <div className="container">
-                <h2 className="mb-5 mt-14 text-balance text-3xl font-bold">
+                <h2 className="mb-5 text-3xl font-bold mt-14 text-balance">
                     Recent Posts
                 </h2>
                 <ul>
@@ -57,9 +61,9 @@ function BlogsPage() {
                         )
                         return (
                             <li key={post.sys.id}>
-                                <div className="mb-8 flex flex-col justify-between rounded bg-orange-300 p-6 shadow-md dark:bg-slate-500 dark:shadow-none">
+                                <div className="flex flex-col justify-between p-6 mb-8 bg-orange-300 rounded shadow-md dark:bg-slate-500 dark:shadow-none">
                                     <div>
-                                        <h2 className="mb-2 text-balance text-xl font-bold ">
+                                        <h2 className="mb-2 text-xl font-bold text-balance ">
                                             {post.fields.title}
                                         </h2>
                                         <p className="text-sm font-semibold text-gray-700 dark:text-gray-300">
@@ -74,7 +78,7 @@ function BlogsPage() {
                                     <Link
                                         to={`/post/${post.sys.id}`}
                                         state={{ post }} // Pass the entire post data through the Link state
-                                        className="mt-2 self-start font-bold text-blue-600 hover:underline dark:text-blue-400"
+                                        className="self-start mt-2 font-bold text-blue-600 hover:underline dark:text-blue-400"
                                     >
                                         Read This Post
                                     </Link>
