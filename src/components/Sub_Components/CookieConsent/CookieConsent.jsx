88643efbfb127645ed585b8310d585d3c711f830
movarnell <michaelvarnell@icloud.com>
2024-10-23 10:34:48 -0500

    Update CookieConsent message for improved user experience

--- src/components/Sub_Components/CookieConsent/CookieConsent.jsx
+++ src/components/Sub_Components/CookieConsent/CookieConsent.jsx
@@ -22,7 +22,7 @@ function CookieConsent() {
     return (
         <div className="cookie-consent-banner">
             <p>
-                We use cookies to enhance your experience. By continuing to visit this site you agree to our use of cookies.
+                We use cookies to enhance your experience, and annoy you less. By continuing to visit this site you agree to our use of cookies.
             </p>
             <button onClick={handleAccept}>Accept</button>
         </div>
