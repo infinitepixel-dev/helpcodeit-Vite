6f688b29e8921dc47e1455b9674b239fc5ffe29c
movarnell <michaelvarnell@icloud.com>
2024-11-12 10:32:04 -0600

    Attempting to fix dependency issues.

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
-/* import { visualizer } from 'rollup-plugin-visualizer' */
-import compression from 'vite-plugin-compression'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
+import compression from 'vite-plugin-compression';
+import path from 'path-browserify';
+import { Buffer } from 'buffer';
 
 // Define __dirname
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
     plugins: [
         react(),
-        /*         visualizer({
-            open: true,
-            gzipSize: true,
-            brotliSize: true,
-            template: 'treemap', // sunburst, treemap, network
-            filename: 'bundle-stats.html',
-        }), */
         compression({
-            algorithm: 'gzip', // Enable gzip compression
+            algorithm: 'gzip',
         }),
     ],
     resolve: {
         alias: {
+            path: 'path-browserify', // Use path-browserify for frontend compatibility
             '@': path.resolve(__dirname, './src'),
             '@assets': path.resolve(__dirname, 'src/assets'),
             '@components': path.resolve(__dirname, 'src/components'),
-            '@subComponents': path.resolve(
-                __dirname,
-                'src/components/Sub_Components'
-            ),
-            '@JSP': path.resolve(
-                __dirname,
-                'src/components/Pages/javascriptPrincipals'
-            ),
+            '@subComponents': path.resolve(__dirname, 'src/components/Sub_Components'),
+            '@JSP': path.resolve(__dirname, 'src/components/Pages/javascriptPrincipals'),
             '@react': path.resolve(__dirname, 'src/components/Pages/react'),
-            '@fundamentals': path.resolve(
-                __dirname,
-                'src/components/Pages/fundamentals'
-            ),
+            '@fundamentals': path.resolve(__dirname, 'src/components/Pages/fundamentals'),
             '@fullcalendar/react': '@fullcalendar/react',
             '@fullcalendar/daygrid': '@fullcalendar/daygrid',
+            'buffer' : 'buffer',
         },
     },
     build: {
@@ -56,4 +42,4 @@ export default defineConfig({
             },
         },
     },
-})
+});
